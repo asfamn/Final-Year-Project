73baab35fa44e391384d38c9566f6e4c29afd462
Amir Asyraaf Aiman <163713594+asfamn@users.noreply.github.com>
2024-06-25 21:53:15 +0800

    Delete cookit_Final directory

--- cookit_Final/config.py
+++
@@ -1,5 +0,0 @@
-RECIPES_PATH = "C:/Users/Kaizen/Desktop/cookit_Final/input/recipe_detail.csv"
-PARSED_PATH = "C:/Users/Kaizen/Desktop/cookit_Final/input/df_parsed.csv"
-
-TFIDF_ENCODING_PATH = "C:/Users/Kaizen/Desktop/cookit_Final/input/tfidf_encodings.pkl"
-TFIDF_MODEL_PATH = "C:/Users/Kaizen/Desktop/cookit_Final/model/tfidf.pkl"
